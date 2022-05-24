c1bb0f2e4a0a5deb8854feca5c27f2add8417155
Suha Siddiqui <suhasid@my.yorku.ca>
2022-05-24 14:36:26 -0400

    some file were skipped in adding here, so the app is possibly faulty-- app was last updated (estimated) october 2021

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ruby on Rails Tutorial: sample application
+
+THis is the sample application for the
+[*Ruby on Rails Tutorial: Learn Web Development with Rails*](http://www.railstutorial.org/)
+by [Suha Siddiqui](https://ca.linkedin.com/in/suha-siddiqui-577492198)
---
+++ test/controllers/model_controller_test.rb
@@ -0,0 +1,19 @@
+# require 'test_helper'
+#
+# class ModelControllerTest < ActionDispatch::IntegrationTest
+#   test "should get User" do
+#     get model_User_url
+#     assert_response :success
+#   end
+#
+#   test "should get name:string" do
+#     get model_name:string_url
+#     assert_response :success
+#   end
+#
+#   test "should get email:string" do
+#     get model_email:string_url
+#     assert_response :success
+#   end
+#
+# end
